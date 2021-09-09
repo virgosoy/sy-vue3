edd804cfb74ff216fcd6cb9d36949bc92189f230
virgosoy <virgosoy>
2021-09-09 14:23:38 +0800

    feat(validRule): 增加工具类valid-rule.js

--- lib/main.ts
+++ lib/main.ts
@@ -1,7 +1,13 @@
+// 组件
 import SyGrid from './components/SyGrid.vue'
 import SyTable from './components/SyTable.vue'
+// 工具类
+import Rule from './utils/valid-rule.js'
 
 export {
+    // 组件
     SyGrid,
     SyTable,
+    // 工具类
+    Rule
 }
