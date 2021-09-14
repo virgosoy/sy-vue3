ceabf3c7f2e4fec0ee4503c85c5e0a4592e15594
virgosoy <virgosoy>
2021-09-14 10:45:21 +0800

    fix: 排除 element-plus 打包依赖

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
         'vue',
         'async-validator',
         'xlsx',
-        'bignumber.js'
+        'bignumber.js',
+        'element-plus'
       ],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
