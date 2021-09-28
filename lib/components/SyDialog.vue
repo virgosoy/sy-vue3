de065d42989d77d47a68ae88ae8d2580d49036ec
virgosoy <virgosoy>
2021-09-28 09:38:29 +0800

    feat(SyDialog): 0.2.0.210928    改为flex布局进行水平垂直居中

--- lib/components/SyDialog.vue
+++ lib/components/SyDialog.vue
@@ -1,11 +1,12 @@
 <template>
     <div :id="id">
-        <div class="sy-dialog__mask"></div>
-        <div class="sy-dialog__box">
-            <div class="sy-dialog__text-area">{{ text }}</div>
-            <div class="sy-dialog__btn-area">
-                <button @click="okHandler()">ok</button>
-                <button @click="cancelHandler()">cancel</button>
+        <div class="sy-dialog__mask">
+            <div class="sy-dialog__box">
+                <div class="sy-dialog__text-area">{{ text }}</div>
+                <div class="sy-dialog__btn-area">
+                    <button @click="okHandler()">ok</button>
+                    <button @click="cancelHandler()">cancel</button>
+                </div>
             </div>
         </div>
     </div>
@@ -14,9 +15,11 @@
 <script>
 /**
  * 对话框模板
- * @version 0.1.0.210127
- * @changelog 见 SyDialogUtil.js
- * @see SyDialogUtil.js
+ * @version 0.2.0.210928    改为flex布局进行水平垂直居中，增加最大宽度避免溢出屏幕，按钮与边框增加间距
+ * @changelog 
+ *          0.2.0.210928    改为flex布局进行水平垂直居中，增加最大宽度避免溢出屏幕，按钮与边框增加间距
+ *          0.1.0.210127    支持简单的文本，确定与取消事件
+ * @see SyDialogUtil.js 两者共用一套版本体系
  * @doc
  *      事件：btnClick
  *          事件对象：
@@ -52,16 +55,10 @@ export default {
 
 <style>
 .sy-dialog__box{
-    --width: 240px;
-    --height: 60px;
-    position: fixed;
-    z-index: 1001;
-    width: var(--width);
-    height: var(--height);
-    left: calc(50% - var(--width) / 2);
-    top: calc(50% - var(--height) / 2);
+    max-width: 100%;
     background: #fff;
     border-radius: 5px;
+    word-break: break-all;
 }
 .sy-dialog__mask{
     background: rgba(0,0,0,0.3);
@@ -71,11 +68,16 @@ export default {
     right: 0;
     bottom: 0;
     z-index: 1000;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
 }
 .sy-dialog__text-area{
     padding: 5px 10px;
 }
 .sy-dialog__btn-area{
+    padding-bottom: 5px;
     text-align: center;
 }
 .sy-dialog__btn-area button{
